8a9e9fa9875bd4a8bdc9de1609f3ec8a6b4aa957
Eyob Worku <you@example.com>
2023-04-30 20:18:13 -0700

    Add js and Css

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-ocument.addEventListener("DOMContentLoaded", function (event) {
-
-    document.getElementById("button1").addEventListener("click", function () {
-
-        document.getElementById("textbox1").value = "success";
-    });
-});
+class Food {
+    constructor(pName, pCalories) {
+      this.name = pName;
+      this.calories = parseInt(pCalories);
+    }
+  }
+  
+  let foodArray = [];
+  const form = document.getElementById("add-food-form");
+  const foodNameInput = document.getElementById("food-name");
+  const caloriesInput = document.getElementById("calories");
+  const totalCalories = document.getElementById("calories-total-value");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const foodName = foodNameInput.value;
+    const calories = caloriesInput.value;
+    const foodObject = new Food(foodName, calories);
+    foodArray.push(foodObject);
+    console.log(foodArray);
+    const sumCalories = foodArray.reduce((total, item) => {
+      return total + item.calories;
+    }, 0);
+    totalCalories.innerText = sumCalories;
+    foodNameInput.value = "";
+    caloriesInput.value = "";
+  });
+  
