242f7ea80b8221a333f2930764db83fc896ca218
apham0816 <anh.t.pham@bellevuecollege.edu>
2023-04-30 19:33:38 -0700

    git status

---
+++ main.js
@@ -0,0 +1,7 @@
+ocument.addEventListener("DOMContentLoaded", function (event) {
+
+    document.getElementById("button1").addEventListener("click", function () {
+
+        document.getElementById("textbox1").value = "success";
+    });
+});
